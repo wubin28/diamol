6cdea69a098b485c836c29e9a0180fb07a7e0d62
wubin28 <wubinben@icloud.com>
2022-09-28 23:35:43 +0800

    test: add a dummy test

---
+++ ch04/exercises/image-of-the-day/src/test/java/iotd/models/ImageTest.java
@@ -0,0 +1,13 @@
+package iotd.models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ImageTest {
+
+    @Test
+    void setUrl() {
+        assertEquals(7, 5 + 2);
+    }
+}
